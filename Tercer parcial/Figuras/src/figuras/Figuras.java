6ac7affd27ffaf4ec6d79d8559eacec5f943d620
MitchAguilar <madialeja06@gmail.com>
2017-05-15 18:27:34 -0500

    figuras

---
+++ Tercer parcial/Figuras/src/figuras/Figuras.java
@@ -0,0 +1,31 @@
+package figuras;
+
+public class Figuras {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static class figuras {
+        
+    }
+    public static class triangulo{
+       private double base, altura,ladoA,ladoB,ladoC;
+       
+    }
+    public static class cuadrado{
+        private double lado;
+    }
+    public static class rectangulo{
+        private double base,altura;
+    }
+    public static class rombo{
+        private double lado,DiagonalMenor,DiagonalMayor;
+    }
+    public static class romboide{
+        private double base,altura;
+    }
+    public static class trapecio{
+        private double lado1,lado2,lado3,lado4,BaseMenor,BaseMayor,Altura;
+    }
+}
